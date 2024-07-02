141622f76e4857d762fcb046be8721f96444c079
Orian <oriangidolcalebou@gmail.com>
2024-07-02 18:13:23 +0300

    Mise en ligne du site educatif

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleVisibility(id) {
+    const element = document.getElementById(id);
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
